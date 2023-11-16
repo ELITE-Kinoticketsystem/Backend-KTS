ffdcec712c928fb54f3d3c1bebde6e17a34e20c2
Hajny25 <daniel.hajnal5@gmail.com>
2023-11-16 21:43:55 +0100

    add db manager and initialization

--- src/main.go
+++ src/main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+	"github.com/ELITE-Kinoticketsystem/Backend-KTS/src/managers"
+)
+
+func main() {
+	dbConnection, _ := managers.InitializeDB()
+	fmt.Print(dbConnection)
+}
