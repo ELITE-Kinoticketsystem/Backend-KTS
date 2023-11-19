2290b260bd3d17bfc2c2ca0f8932c8db58276780
kieberts <sebastian.kiebert@sap.com>
2023-11-19 15:42:02 +0100

    Declare all needed databse methods

--- src/repositories/movie_repository.go
+++ src/repositories/movie_repository.go
@@ -1,1 +1,29 @@
 package repositories
+
+import (
+	"context"
+
+	"github.com/ELITE-Kinoticketsystem/Backend-KTS/src/managers"
+	"github.com/ELITE-Kinoticketsystem/Backend-KTS/src/models"
+	"github.com/google/uuid"
+)
+
+type TripRepo interface {
+	GetMovies(ctx context.Context) []*models.Movie
+	
+	// CRUD
+	GetMovieById(ctx context.Context, movieId *uuid.UUID) *models.Movie
+	CreateMovie(ctx context.Context, movie *models.Movie) *models.Movie
+	UpdateMovie(ctx context.Context, movie *models.Movie) *models.Movie
+	DeleteMovie(ctx context.Context, movieId *uuid.UUID) *models.Movie
+
+	// Validation
+	ValidateIfMovieExists(ctx context.Context, movieId *uuid.UUID) bool
+
+	GetActorsFromMovie(ctx context.Context, movieId *uuid.UUID) []*models.Actor
+	GetProducerFromMovie(ctx context.Context, movieId *uuid.UUID) *models.Producer
+}
+
+type TripRepository struct {
+	DatabaseMgr managers.DatabaseManagerI
+}
