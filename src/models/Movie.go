26291058a930531029a0131f17e1cc994eca6add
kieberts <sebastian.kiebert@sap.com>
2023-11-19 16:44:03 +0100

    Updated Movie model due to db table

--- src/models/Movie.go
+++ src/models/Movie.go
@@ -1,15 +1,18 @@
 package models
 
 type Movie struct {
+	Id          string      `json:"id"`
 	Title       string      `json:"title"`
 	Description string      `json:"description"`
 	ReleaseDate string      `json:"releaseDate"`
 	TimeInMin   int         `json:"timeInMin"`
 	FSK         FSK         `json:"fsk"`
+	Genre       Genre       `json:"genre"`
 	Producers   *[]Producer `json:"producers"`
 	Actors      *[]Actor    `json:"actors"`
 }
 
+type Genre string
 type FSK string
 
 const (
@@ -18,4 +21,11 @@ const (
 	Twelve   FSK = "FSK ab 12 freigegeben"
 	Sixteen  FSK = "FSK ab 16 freigegeben"
 	Eighteen FSK = "FSK ab 18 freigegeben"
+
+	Action  Genre = "Action"
+	Drama   Genre = "Drama"
+	Crime   Genre = "Crime"
+	Fantasy Genre = "Fantasy"
+	Western Genre = "Western"
+	Romance Genre = "Romance"
 )
