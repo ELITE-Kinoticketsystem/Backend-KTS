13d3e96d8aef4188e44c1dbf7b4b278d0604d7c4
Hajny25 <daniel.hajnal5@gmail.com>
2023-11-22 16:07:58 +0100

    update schemas

--- src/models/schemas/Event.go
+++ src/models/schemas/Event.go
@@ -9,16 +9,47 @@ import (
 type Event struct {
 	Id           *uuid.UUID `json:"id"`
 	Title        string     `json:"title"`
-	EventType    EventType  `json:"eventType"`
 	Start        time.Time  `json:"start"`
 	End          time.Time  `json:"end"`
-	Price        int        `json:"price"` // requires conversion
+	EventTypeId  *uuid.UUID `json:"eventTypeId"`
 	CinemaHallId *uuid.UUID `json:"cinemaHallId"`
 }
 
-type EventType string
+type EventType struct {
+	Id       *uuid.UUID `json:"id"`
+	TypeName string     `json:"typeName"`
+}
 
-const (
-	Showing      EventType = "Showing"
-	SpecialEvent EventType = "SpecialEvent"
-)
+type EventMovie struct {
+	EventId *uuid.UUID `json:"eventId"`
+	MovieId *uuid.UUID `json:"movieId"`
+}
+
+type Ticket struct {
+	Id              *uuid.UUID `json:"id"`
+	Validated       bool       `json:"validated"`
+	Price           int        `json:"price"` // requires conversion
+	PriceCategoryId *uuid.UUID `json:"priceCategoryId"`
+	OrderId         *uuid.UUID `json:"orderId"`
+	EventSeatId     *uuid.UUID `json:"eventSeatId"`
+}
+
+type PriceCategory struct {
+	CategoryName string `json:"categoryName"`
+	Price        int    `json:"price"`
+}
+
+type EventSeatCategory struct {
+	EventId        *uuid.UUID
+	SeatCategoryId *uuid.UUID
+	Price          int
+}
+
+type EventSeat struct {
+	Id           *uuid.UUID
+	Booked       bool
+	BlockedUntil time.Time
+	UserId       *uuid.UUID
+	SeatId       *uuid.UUID
+	EventId      *uuid.UUID
+}
