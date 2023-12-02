7a6db71babc11d537c8612bec30489c96d2c6c62
kieberts <sebastian@kiebert.com>
2023-12-02 18:27:47 +0100

    Added GET Genre functions

--- src/repositories/movie_repository.go
+++ src/repositories/movie_repository.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/ELITE-Kinoticketsystem/Backend-KTS/src/.gen/KinoTicketSystem/table"
 	kts_errors "github.com/ELITE-Kinoticketsystem/Backend-KTS/src/errors"
 
+	"github.com/ELITE-Kinoticketsystem/Backend-KTS/src/.gen/KinoTicketSystem/model"
 	"github.com/ELITE-Kinoticketsystem/Backend-KTS/src/managers"
 	"github.com/ELITE-Kinoticketsystem/Backend-KTS/src/models"
 	"github.com/ELITE-Kinoticketsystem/Backend-KTS/src/models/schemas"
@@ -19,10 +20,14 @@ type MovieRepoI interface {
 	UpdateMovie(movie *schemas.Movie) *models.KTSError
 	DeleteMovie(id *uuid.UUID) *models.KTSError
 
-	GetGenreByName(name string) (*schemas.Genre, *models.KTSError)
-	CreateGenre(genre *schemas.Genre) *models.KTSError
+	GetGenreByName(name string) (*model.Genres, *models.KTSError)
+	CreateGenre(genre *model.Genres) *models.KTSError
+	GetGenres() (*[]model.Genres, *models.KTSError)
 
 	AddMovieGenre(movieId *uuid.UUID, genreId *uuid.UUID) *models.KTSError
+
+	GetMoviesByGenre(genreId *uuid.UUID) (*[]model.Movies, *models.KTSError)
+	GetMoviesByGenres() (*[]model.Movies, *models.KTSError)
 }
 
 type MovieRepository struct {
@@ -33,15 +38,53 @@ func (mr *MovieRepository) AddMovieGenre(movieId *uuid.UUID, genreId *uuid.UUID)
 	// TODO: implement
 	return nil
 }
+func (mr *MovieRepository) GetGenreByName(name string) (*model.Genres, *models.KTSError) {
+	var genre model.Genres
 
-func (mr *MovieRepository) CreateGenre(genre *schemas.Genre) *models.KTSError {
-	// TODO: implement
-	return nil
+	// Create the query
+	stmt := SELECT(
+		Genres.AllColumns,
+	).FROM(
+		Genres,
+	).WHERE(
+		Genres.GenreName.EQ(String(name)),
+	)
+
+	// Execute the query
+	err := stmt.Query(mr.DatabaseManager.GetDatabaseConnection(), &genre)
+	if err != nil {
+		return nil, kts_errors.KTS_INTERNAL_ERROR
+	}
+
+	return &genre, nil
 }
 
-func (mr *MovieRepository) GetGenreByName(name string) (*schemas.Genre, *models.KTSError) {
-	// TODO implement
-	return nil, nil
+func (mr *MovieRepository) GetGenres() (*[]model.Genres, *models.KTSError) {
+	var genres []model.Genres
+
+	// Create the query
+	stmt := SELECT(
+		Genres.AllColumns,
+	).FROM(
+		Genres,
+	)
+
+	// Execute the query
+	err := stmt.Query(mr.DatabaseManager.GetDatabaseConnection(), &genres)
+	if err != nil {
+		return nil, kts_errors.KTS_INTERNAL_ERROR
+	}
+
+	if len(genres) == 0 {
+		return nil, kts_errors.KTS_NOT_FOUND
+	}
+
+	return &genres, nil
+}
+
+func (mr *MovieRepository) CreateGenre(genre *model.Genres) *models.KTSError {
+	// TODO: implement
+	return nil
 }
 
 func (mr *MovieRepository) CreateMovie(movie *schemas.Movie) *models.KTSError {
@@ -109,4 +152,54 @@ func (mr *MovieRepository) UpdateMovie(movie *schemas.Movie) *models.KTSError {
 func (mr *MovieRepository) DeleteMovie(id *uuid.UUID) *models.KTSError {
 	// TODO: implement
 	return nil
+}
+
+func (mr *MovieRepository) GetMoviesByGenre(genreName string) (*models.GenresWithMovie, *models.KTSError) {
+	var movies models.GenresWithMovie
+
+	// Create the query
+	stmt := SELECT(
+		Movies.AllColumns,
+		Genres.AllColumns,
+	).FROM(
+		MovieGenres.
+			INNER_JOIN(Movies, Movies.ID.EQ(MovieGenres.MovieID)).
+			INNER_JOIN(Genres, Genres.ID.EQ(MovieGenres.GenreID)),
+	).WHERE(
+		Genres.GenreName.EQ(String(genreName)),
+	)
+
+	// Execute the query
+	err := stmt.Query(mr.DatabaseManager.GetDatabaseConnection(), &movies)
+	if err != nil {
+		return nil, kts_errors.KTS_INTERNAL_ERROR
+	}
+
+	return &movies, nil
+}
+
+func (mr *MovieRepository) GetMoviesByGenres() (*[]models.GenresWithMovie, *models.KTSError) {
+	var genresWithMovies []models.GenresWithMovie
+
+	// Create the query
+	stmt := SELECT(
+		Movies.AllColumns,
+		Genres.AllColumns,
+	).FROM(
+		MovieGenres.
+			INNER_JOIN(Movies, Movies.ID.EQ(MovieGenres.MovieID)).
+			INNER_JOIN(Genres, Genres.ID.EQ(MovieGenres.GenreID)),
+	)
+
+	// Execute the query
+	err := stmt.Query(mr.DatabaseManager.GetDatabaseConnection(), &genresWithMovies)
+	if err != nil {
+		return nil, kts_errors.KTS_INTERNAL_ERROR
+	}
+
+	if len(genresWithMovies) == 0 {
+		return nil, kts_errors.KTS_NOT_FOUND
+	}
+
+	return &genresWithMovies, nil
 }
