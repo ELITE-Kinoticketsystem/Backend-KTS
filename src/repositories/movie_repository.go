1af1181921c476655055df2c8c75bcc02d5417f1
kieberts <sebastian@kiebert.com>
2023-12-07 11:27:43 +0100

    Extract Genre and MovieGenre to its own files

--- src/repositories/movie_repository.go
+++ src/repositories/movie_repository.go
@@ -1,8 +1,6 @@
 package repositories
 
 import (
-	"log"
-
 	"github.com/ELITE-Kinoticketsystem/Backend-KTS/src/.gen/KinoTicketSystem/table"
 	kts_errors "github.com/ELITE-Kinoticketsystem/Backend-KTS/src/errors"
 
@@ -22,21 +20,8 @@ type MovieRepositoryI interface {
 	UpdateMovie(movie model.Movies) *models.KTSError
 	DeleteMovie(movieId *uuid.UUID) *models.KTSError
 
-	// Genre
-	GetGenres() (*[]model.Genres, *models.KTSError)
-	GetGenreByName(name string) (*model.Genres, *models.KTSError)
-	CreateGenre(name string) *models.KTSError
-
-	// Combine Movie and Genre
-	AddMovieGenre(movieId *uuid.UUID, genreId *uuid.UUID) *models.KTSError
-	RemoveMovieGenre(movieId *uuid.UUID, genreId *uuid.UUID) *models.KTSError
-
 	// One Movie with all Genres
-	GetMovieByIdWithGenre(movieId *uuid.UUID) (*models.MovieWithGenres, *models.KTSError)
-	// One Genre with all Movies
-	GetGenreByNameWithMovies(genreName string) (*models.GenreWithMovies, *models.KTSError)
-	// All Movies with all Genres - Grouped by Genre
-	GetGenresWithMovies() (*[]models.GenreWithMovies, *models.KTSError)
+	GetMovieByIdWithGenre(movieId *uuid.UUID) (*models.MovieWithGenres, *models.KTSError)	
 	// All Movies with all Genres - Grouped by Movie
 	GetMoviesWithGenres() ([]models.MovieWithGenres, *models.KTSError)
 }
@@ -171,115 +156,6 @@ func (mr *MovieRepository) DeleteMovie(movieId *uuid.UUID) *models.KTSError {
 	return nil
 }
 
-// Genre
-func (mr *MovieRepository) GetGenres() (*[]model.Genres, *models.KTSError) {
-	var genres []model.Genres
-
-	// Create the query
-	stmt := jet_mysql.SELECT(
-		table.Genres.AllColumns,
-	).FROM(
-		table.Genres,
-	)
-
-	// Execute the query
-	err := stmt.Query(mr.DatabaseManager.GetDatabaseConnection(), &genres)
-	if err != nil {
-		return nil, kts_errors.KTS_INTERNAL_ERROR
-	}
-
-	if len(genres) == 0 {
-		return nil, kts_errors.KTS_NOT_FOUND
-	}
-
-	return &genres, nil
-}
-
-func (mr *MovieRepository) GetGenreByName(name string) (*model.Genres, *models.KTSError) {
-	var genre model.Genres
-
-	// Create the query
-	stmt := jet_mysql.SELECT(
-		table.Genres.AllColumns,
-	).FROM(
-		table.Genres,
-	).WHERE(
-		table.Genres.GenreName.EQ(jet_mysql.String(name)),
-	)
-
-	// Execute the query
-	err := stmt.Query(mr.DatabaseManager.GetDatabaseConnection(), &genre)
-	if err != nil {
-		return nil, kts_errors.KTS_INTERNAL_ERROR
-	}
-
-	return &genre, nil
-}
-
-func (mr *MovieRepository) CreateGenre(name string) *models.KTSError {
-	// Create the insert statement
-	insertQuery := table.Genres.INSERT(table.Genres.GenreName).
-		VALUES(name)
-
-	// Execute the query
-	_, err := insertQuery.Exec(mr.DatabaseManager.GetDatabaseConnection())
-	if err != nil {
-		return kts_errors.KTS_INTERNAL_ERROR
-	}
-
-	return nil
-}
-
-// Combine Movie and Genre
-func (mr *MovieRepository) AddMovieGenre(movieId *uuid.UUID, genreId *uuid.UUID) *models.KTSError {
-
-	binary_movie_id, _ := movieId.MarshalBinary()
-	binary_genre_id, _ := genreId.MarshalBinary()
-
-	// Create the insert statement
-	insertQuery := table.MovieGenres.INSERT(table.MovieGenres.MovieID, table.MovieGenres.GenreID).
-		VALUES(jet_mysql.String(string(binary_movie_id)), jet_mysql.String(string(binary_genre_id)))
-
-	// Execute the query
-	_, err := insertQuery.Exec(mr.DatabaseManager.GetDatabaseConnection())
-	if err != nil {
-		return kts_errors.KTS_INTERNAL_ERROR
-	}
-
-	return nil
-}
-
-func (mr *MovieRepository) RemoveMovieGenre(movieId *uuid.UUID, genreId *uuid.UUID) *models.KTSError {
-
-	// Create the delete statement
-	deleteQuery := table.MovieGenres.DELETE().WHERE(
-		table.MovieGenres.MovieID.EQ(
-			jet_mysql.CAST(jet_mysql.String(movieId.String())).AS_BINARY(),
-		).AND(
-			table.MovieGenres.GenreID.EQ(
-				jet_mysql.CAST(jet_mysql.String(genreId.String())).AS_BINARY(),
-			),
-		),
-	)
-
-	// Execute the query
-	rows, err := deleteQuery.Exec(mr.DatabaseManager.GetDatabaseConnection())
-	if err != nil {
-		return kts_errors.KTS_INTERNAL_ERROR
-	}
-
-	rowsAff, err := rows.RowsAffected()
-	if err != nil {
-		return kts_errors.KTS_INTERNAL_ERROR
-	}
-
-	if rowsAff == 0 {
-		return kts_errors.KTS_NOT_FOUND
-	}
-
-	return nil
-}
-
 // One Movie with all Genres
 func (mr *MovieRepository) GetMovieByIdWithGenre(movieId *uuid.UUID) (*models.MovieWithGenres, *models.KTSError) {
 	var movie models.MovieWithGenres
@@ -305,61 +181,6 @@ func (mr *MovieRepository) GetMovieByIdWithGenre(movieId *uuid.UUID) (*models.Mo
 	}
 
 	return &movie, nil
-}
-
-// One Genre with all table.Movies
-func (mr *MovieRepository) GetGenreByNameWithMovies(genreName string) (*models.GenreWithMovies, *models.KTSError) {
-	var movies models.GenreWithMovies
-
-	// Create the query
-	stmt := jet_mysql.SELECT(
-		table.Movies.AllColumns,
-		table.Genres.AllColumns,
-	).FROM(
-		table.MovieGenres.
-			INNER_JOIN(table.Movies, table.Movies.ID.EQ(table.MovieGenres.MovieID)).
-			INNER_JOIN(table.Genres, table.Genres.ID.EQ(table.MovieGenres.GenreID)),
-	).WHERE(
-		table.Genres.GenreName.EQ(jet_mysql.String(genreName)),
-	)
-
-	log.Print(stmt.DebugSql())
-
-	// Execute the query
-	err := stmt.Query(mr.DatabaseManager.GetDatabaseConnection(), &movies)
-	if err != nil {
-		return nil, kts_errors.KTS_INTERNAL_ERROR
-	}
-
-	return &movies, nil
-}
-
-// All Movies with all Genres - Grouped by Genre
-func (mr *MovieRepository) GetGenresWithMovies() (*[]models.GenreWithMovies, *models.KTSError) {
-	var genresWithMovies []models.GenreWithMovies
-
-	// Create the query
-	stmt := jet_mysql.SELECT(
-		table.Movies.AllColumns,
-		table.Genres.AllColumns,
-	).FROM(
-		table.MovieGenres.
-			INNER_JOIN(table.Movies, table.Movies.ID.EQ(table.MovieGenres.MovieID)).
-			INNER_JOIN(table.Genres, table.Genres.ID.EQ(table.MovieGenres.GenreID)),
-	)
-
-	log.Print(stmt.DebugSql())
-	// Execute the query
-	err := stmt.Query(mr.DatabaseManager.GetDatabaseConnection(), &genresWithMovies)
-	if err != nil {
-		return nil, kts_errors.KTS_INTERNAL_ERROR
-	}
-
-	if len(genresWithMovies) == 0 {
-		return nil, kts_errors.KTS_NOT_FOUND
-	}
-
-	return &genresWithMovies, nil
 }
 
 // All Movies with all Genres - Grouped by Movie
