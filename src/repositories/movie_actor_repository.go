03c7012278b1d51be29ff8493d4d0779451d25bf
kieberts <sebastian@kiebert.com>
2023-12-13 16:31:54 +0100

    Implemented and testet movie_actor repo

---
+++ src/repositories/movie_actor_repository.go
@@ -0,0 +1,71 @@
+package repositories
+
+import (
+	"github.com/ELITE-Kinoticketsystem/Backend-KTS/src/.gen/KinoTicketSystem/table"
+	kts_errors "github.com/ELITE-Kinoticketsystem/Backend-KTS/src/errors"
+	"github.com/ELITE-Kinoticketsystem/Backend-KTS/src/managers"
+	"github.com/ELITE-Kinoticketsystem/Backend-KTS/src/models"
+	"github.com/ELITE-Kinoticketsystem/Backend-KTS/src/utils"
+	"github.com/google/uuid"
+)
+
+type MovieActorRepositoryI interface {
+	// Combine Movie and Actor
+	AddMovieActor(movieId *uuid.UUID, actorId *uuid.UUID) *models.KTSError
+	RemoveMovieActor(movieId *uuid.UUID, actorId *uuid.UUID) *models.KTSError
+}
+
+type MovieActorRepository struct {
+	DatabaseManager managers.DatabaseManagerI
+}
+
+// Combine Movie and Genre
+func (ar *MovieActorRepository) AddMovieActor(movieId *uuid.UUID, actorId *uuid.UUID) *models.KTSError {
+
+	// Create the insert statement
+	insertQuery := table.MovieActors.INSERT(table.MovieActors.MovieID, table.MovieActors.ActorID).
+		VALUES(utils.MysqlUuid(movieId), utils.MysqlUuid(actorId))
+
+	// Execute the query
+	rows, err := insertQuery.Exec(ar.DatabaseManager.GetDatabaseConnection())
+	if err != nil {
+		return kts_errors.KTS_INTERNAL_ERROR
+	}
+
+	rowsAff, err := rows.RowsAffected()
+	if err != nil {
+		return kts_errors.KTS_INTERNAL_ERROR
+	}
+
+	if rowsAff == 0 {
+		return kts_errors.KTS_NOT_FOUND
+	}
+
+	return nil
+}
+
+func (ar *MovieActorRepository) RemoveMovieActor(movieId *uuid.UUID, actorId *uuid.UUID) *models.KTSError {
+
+	deleteQuery := table.MovieGenres.DELETE().WHERE(
+		table.MovieActors.MovieID.EQ(utils.MysqlUuid(movieId)).AND(
+			table.MovieActors.ActorID.EQ(utils.MysqlUuid(actorId)),
+		),
+	)
+
+	// Execute the query
+	rows, err := deleteQuery.Exec(ar.DatabaseManager.GetDatabaseConnection())
+	if err != nil {
+		return kts_errors.KTS_INTERNAL_ERROR
+	}
+
+	rowsAff, err := rows.RowsAffected()
+	if err != nil {
+		return kts_errors.KTS_INTERNAL_ERROR
+	}
+
+	if rowsAff == 0 {
+		return kts_errors.KTS_NOT_FOUND
+	}
+
+	return nil
+}
