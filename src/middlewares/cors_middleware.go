0cec8c24969d3a93563a3ea94a01b865e779a6ff
Collin Forslund <I570043@MBP-Collin.dhcp.wdf.sap.corp>
2023-12-15 16:31:21 +0100

    fix cors maybe

--- src/middlewares/cors_middleware.go
+++ src/middlewares/cors_middleware.go
@@ -6,6 +6,12 @@ import (
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+
+		c.Header("Access-Control-Allow-Methods", "GET, PATCH, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
 		c.Next()
 	}
 }
