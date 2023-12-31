c53af57e007dd6c51d7cb964fe6b6d8d9679518a
Sebastian Kiebert <65255153+Kieberts@users.noreply.github.com>
2023-12-31 19:48:52 +0100

    Remove commands

--- src/models/ticketmodels.go
+++ src/models/ticketmodels.go
@@ -5,19 +5,6 @@ import (
 	"github.com/google/uuid"
 )
 
-// type testStruct struct {
-// 	model.Tickets
-
-// 	EventSeats model.EventSeats
-// }
-
-// type TicketDTOValidate struct {
-// 	ID        *uuid.UUID
-// 	Validated bool
-// 	Price     int32
-// 	IsPaid    bool
-// }
-
 type TicketDTO struct {
 	ID        *uuid.UUID `sql:"primary_key" alias:"ticket.id"`
 	Validated bool       `alias:"ticket.validated"`
@@ -27,11 +14,3 @@ type TicketDTO struct {
 	Order *model.Orders
 	Event *model.Events
 }
-
-// type TicketDTO struct {
-// 	model.Tickets
-
-// 	Seats struct{ model.Seats }
-// 	Order struct{ model.Orders }
-// 	Event struct{ model.Events }
-// }
