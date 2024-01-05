37aa312825e7572cb38be1f8b12182d8b800928f
Collin Forslund <I570043@MBP-Collin.dhcp.wdf.sap.corp>
2024-01-05 07:52:51 +0100

    make price calc better

--- src/utils/priceutils.go
+++ src/utils/priceutils.go
@@ -1,5 +1,5 @@
 package utils
 
 func CalculatePrice(seatPrice int32, priceCategoryDiscount int32) int32 {
-	return seatPrice - (seatPrice * priceCategoryDiscount / 100)
+	return seatPrice * (1 - priceCategoryDiscount / 100)
 }
