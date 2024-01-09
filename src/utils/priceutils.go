a230f26dae57515fa523df028443d72cb8f2fe0e
cll1n <I570043@MacBook-Pro-Collin.local>
2024-01-09 15:51:16 +0100

    make order price calc better

--- src/utils/priceutils.go
+++ src/utils/priceutils.go
@@ -1,7 +1,9 @@
 package utils
 
 func CalculatePrice(seatPrice int32, priceCategoryDiscount int32, seatType string) int32 {
-	singleSeatPrice := seatPrice * (1 - priceCategoryDiscount/100)
+	test := float64(priceCategoryDiscount)
+	discount := 1. - test/100.
+	singleSeatPrice := int32(float64(seatPrice) * discount)
 	if seatType == string(DOUBLE) {
 		return singleSeatPrice * 2
 	}
